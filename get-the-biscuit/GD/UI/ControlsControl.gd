0c852653fea6828c82dd12e499a0871a83a093bd
Alex <alexander.james.swanson@gmail.com>
2025-08-30 05:31:53 -0400

    player can check their controls

---
+++ get-the-biscuit/GD/UI/ControlsControl.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	GameManager.isPaused = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_quit_button_up() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	GameManager.isPaused = false
+	queue_free()
+	pass # Replace with function body.
